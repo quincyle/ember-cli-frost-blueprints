3d0c3087c47c69c9d69d68b52919503cf029d844
Quincy Le <lexuqing8@gmail.com>
2017-11-22 00:37:37 -0500

    update-ciena-config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,3 @@
 module.exports = {
-  extends: ['frost-standard'],
-  rules: {
-    'ocd/sort-import-declarations': [
-      2,
-      {
-        localPrefixes: [
-          '../',
-          './',
-          'dummy/',
-          'ember-cli-frost-blueprints'
-        ]
-      }
-    ],
-    'mocha/no-mocha-arrows': 2,
-    'mocha/valid-test-description': 2
-  }
+  extends: 'frost-standard'
 }
